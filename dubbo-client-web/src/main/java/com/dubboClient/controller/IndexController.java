d3e69a6dede9623ad461b88cf1ca8dd11a11dc82
xuliangxiao <liangxiao.xu@eascs.com>
2017-06-14 15:03:21 +0800

    最基本的客户端，其他的配置都是默认的

---
+++ dubbo-client-web/src/main/java/com/dubboClient/controller/IndexController.java
@@ -0,0 +1,31 @@
+package com.dubboClient.controller;
+
+import com.dubboServer.service.CommonService;
+import com.dubboServer.vo.req.UserReq;
+import com.dubboServer.vo.res.UserRes;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by xuliangxiao on 2017/6/12 14:40
+ */
+@Controller
+public class IndexController {
+    private static Log logger = LogFactory.getLog(IndexController.class);
+
+    @Autowired
+    private CommonService commonService;
+    @RequestMapping("index")
+    public String index(HttpServletRequest request, HttpServletResponse response){
+        logger.info("this is index.html");
+        UserRes userRes = commonService.getUser(new UserReq("xiaoming", 0));
+        logger.info(userRes.toString());
+        return "index";
+    }
+}
